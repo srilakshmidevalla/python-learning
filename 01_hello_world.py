f7181a958d97e84f8aacb52c02fa3e33bbe785da
Devalla Srilakshmi <srilakshmidevalla14@gmail.com>
2026-03-29 03:02:15 -0700

    Add basic Python hello world program

---
+++ 01_hello_world.py
@@ -0,0 +1,16 @@
+ 01_hello_world.py
+# Your very first Python program!
+# Run: python 01_basics/01_hello_world.py
+ 
+# print() displays text to the screen
+print("Hello, World!")
+print("Welcome to Python!")
+ 
+# You can print numbers too
+print(42)
+print(3.14)
+ 
+# Combine text using + (for strings)
+name = "Python"
+print("Hello, " + name + "!")
+ 
